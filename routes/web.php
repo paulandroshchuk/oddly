b2c02366f0b2dfc94c0a10092d2ea77380afa6d8
Paul A <impaul@hey.com>
2024-01-30 23:17:36 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('/dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
